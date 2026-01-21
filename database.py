e9da3d8ab39eebd189faf49c470761c812dd1def
Giuly <giulia.sironi.02@gmail.com>
2026-01-21 17:25:26 +0100

    Update database.py

--- database.py
+++ database.py
@@ -1,6 +1,9 @@
 import os
 import psycopg
 from datetime import datetime
+from dotenv import load_dotenv
+import json
+load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
@@ -15,27 +18,84 @@ def init_db():
     with get_conn() as conn:
         with conn.cursor() as cur:
             cur.execute("""
-            CREATE TABLE IF NOT EXISTS cases (
+            DROP TABLE IF EXISTS ear_cases;
+            """)
+            cur.execute("""
+            CREATE TABLE ear_cases (
                 id TEXT PRIMARY KEY,
-                scan_filename TEXT,
-                scan_data BYTEA,
 
-                x_min REAL,
-                x_max REAL,
-                y_min REAL,
-                y_max REAL,
-                z_min REAL,
-                z_max REAL,
+                is_left BOOLEAN NOT NULL,
+                is_right BOOLEAN NOT NULL,
+
+                original_model_url TEXT NOT NULL,
+                generated_model_url TEXT NOT NULL,
+
+                roi_vertices JSONB NOT NULL,
+                roi_faces JSONB NOT NULL,
 
                 volume_mm3 REAL,
+                watertight BOOLEAN,
 
-                classification TEXT,
+                is_pathological BOOLEAN NOT NULL,
+                is_non_pathological BOOLEAN NOT NULL,
+                is_other BOOLEAN NOT NULL,
                 other_text TEXT,
-                comment TEXT,
 
                 created_at TIMESTAMP DEFAULT NOW()
-            )
+            );
             """)
+        conn.commit()
+
+
+def insert_ear_case(
+    case_id,
+    is_left,
+    is_right,
+    original_model_url,
+    generated_model_url,
+    roi_vertices,
+    roi_faces,
+    volume_mm3,
+    watertight,
+    is_pathological,
+    is_non_pathological,
+    is_other,
+    other_text
+):
+    with get_conn() as conn:
+        with conn.cursor() as cur:
+            cur.execute("""
+            INSERT INTO ear_cases (
+                id,
+                is_left,
+                is_right,
+                original_model_url,
+                generated_model_url,
+                roi_vertices,
+                roi_faces,
+                volume_mm3,
+                watertight,
+                is_pathological,
+                is_non_pathological,
+                is_other,
+                other_text
+            )
+            VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
+            """, (
+                case_id,
+                is_left,
+                is_right,
+                original_model_url,
+                generated_model_url,
+                json.dumps(roi_vertices),
+                json.dumps(roi_faces),
+                volume_mm3,
+                watertight,
+                is_pathological,
+                is_non_pathological,
+                is_other,
+                other_text
+            ))
         conn.commit()
 
 
